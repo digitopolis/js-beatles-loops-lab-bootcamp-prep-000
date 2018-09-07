6e6bc838cefbf2fdd089db185368c346e72afb20
Matt Readout <matt.readout@gmail.com>
2018-09-07 21:20:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,7 +20,8 @@ function johnLennonFacts(facts) {
 function iLoveTheBeatles(number) {
   var beatlesArray = [];
   do {
-    
+    beatlesArray.push('I love the Beatles!');
+    number++;
   }
   while (number < 15);
 }
