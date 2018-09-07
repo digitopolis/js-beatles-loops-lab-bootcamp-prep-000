38a47870a817766b34f3c8e6e9e26a46e26c7b85
Matt Readout <matt.readout@gmail.com>
2018-09-07 20:46:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // add solution here
 function theBeatlesPlay(musicians, instruments) {
   var theBand = [];
+  var i;
   for (i = 0; i < 3; i++) {
     theBand.push(musicians[i] + ' plays ' + instruments[i])
   }
