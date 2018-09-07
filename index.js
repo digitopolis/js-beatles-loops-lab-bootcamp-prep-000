d4858948273525b13cf61f746e98280359c7c2ae
Matt Readout <matt.readout@gmail.com>
2018-09-07 20:46:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 // add solution here
 function theBeatlesPlay(musicians, instruments) {
   var theBand = [];
-  for ()
+  for (i = 0; i < 3; i++) {
+    theBand.push(musicians[i] + ' plays ' + instruments[i])
+  }
 }
 
 function johnLennonFacts(facts) {
